6696d4d69d307853c0e94f04004c9ecbd79caf2d
ngohongthong <baphongpine@gmail.com>
2023-06-21 22:43:46 +0700

    add file.bat to reduce time to migrate

--- app/init_db.bat
+++ app/init_db.bat
@@ -1,1 +1,16 @@
-py .\init_db.py
+@echo off
+del /F db.sqlite3
+del main\migrations\0001_initial.py
+
+call python manage.py makemigrations
+
+call python manage.py migrate
+
+call python init_db.py
+
+rmdir /s /q main\migrations\__pycache__
+rmdir /s /q main\__pycache__
+rmdir /s /q Vis4T_main\__pycache__
+
+
+call python manage.py createsuperuser
